2519712ae306eb0aebee66fb335e95e36872c8b9
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-01 13:56:39 -0800

    Add classNames for styling

--- emilycorley/src/App.js
+++ emilycorley/src/App.js
@@ -8,7 +8,7 @@ import './Styles/app.css'
 
 function App() {
   return (
-    <div className="app">
+    <body className="body">
    <Navbar />
     <Routes>
        <Route path="/" element={<About />}/> 
@@ -16,7 +16,7 @@ function App() {
        <Route path="/personal" element={<PersonalProjects />}/> 
        <Route path="/contact" element={<Contact />}/> 
     </Routes>
-    </div>
+    </body>
   );
 }
 
