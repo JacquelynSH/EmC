fb45f1a72ea285ca5980efec6119ceca0460a43f
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-01 13:58:44 -0800

    Add unordered list to display articles with links

--- emilycorley/src/views/Work.js
+++ emilycorley/src/views/Work.js
@@ -1,7 +1,26 @@
+import '../Styles/work.css'
+
 function Work() {
- return (
-  <div>WORK</div>
- )
+  return (
+    <main className="work-container">
+      <h3>Short stories</h3>
+      <li>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+      </li>
+      <h3>Music articles</h3>
+      <li>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+        <ul><a href="https://www.google.com/">Name of Article</a>, Publication</ul>
+      </li>
+    </main>
+  )
 };
 
 export default Work;
