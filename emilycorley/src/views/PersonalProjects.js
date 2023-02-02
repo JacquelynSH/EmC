30c6ecc7a8ccf85f9cc68b2e1e5e86f21d1645e9
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-01 16:43:13 -0800

    Create CSS file for personal porjects/Page layout

--- emilycorley/src/views/PersonalProjects.js
+++ emilycorley/src/views/PersonalProjects.js
@@ -1,6 +1,11 @@
+import '../Styles/PersonalProjects.css';
+
 function PersonalProjects() {
   return (
-    <div>links here</div>
+    <main className="personal-container">
+      <div>Title of Piece</div>
+      <p>Description of piece, am I a poem? am I a story?</p>
+    </main>
   )
 };
 
